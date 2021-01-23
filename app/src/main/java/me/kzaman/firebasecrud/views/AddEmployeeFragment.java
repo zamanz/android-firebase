58d4c325015a417fcd31b9516658cea7b5f72996
KAMRUZZAMAN <kzamanweb@gmail.com>
2021-01-23 16:50:14 +0600

    add employee on firestore

--- app/src/main/java/me/kzaman/firebasecrud/views/AddEmployeeFragment.java
+++ app/src/main/java/me/kzaman/firebasecrud/views/AddEmployeeFragment.java
@@ -2,6 +2,7 @@ package me.kzaman.firebasecrud.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,12 +12,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import me.kzaman.firebasecrud.R;
 
 public class AddEmployeeFragment extends Fragment {
-    
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     Button save_btn;
     TextInputLayout name, email, phone;
 
@@ -44,14 +53,34 @@ public class AddEmployeeFragment extends Fragment {
         email = (TextInputLayout) view.findViewById(R.id.emailInputField);
         phone = (TextInputLayout) view.findViewById(R.id.phoneInputField);
 
-        //String StrName = name.getEditText().getText().toString();
-        //String StrEmail = email.getEditText().getText().toString();
-        //String StrPhone = phone.getEditText().getText().toString();
+        // Create a new user with a first and last name
+
+
 
         save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), name.getEditText().getText().toString(), Toast.LENGTH_SHORT).show();
+                //set value
+                Map<String, Object> employee = new HashMap<>();
+                employee.put("name", name.getEditText().getText().toString());
+                employee.put("email", email.getEditText().getText().toString());
+                employee.put("corporate_number", phone.getEditText().getText().toString());
+
+                // Add a new document with a generated ID
+                db.collection("employees")
+                    .add(employee)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(getContext(), "DocumentSnapshot added with ID: " + documentReference.getId(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Error adding document" + e, Toast.LENGTH_LONG).show();
+                        }
+                    });
             }
         });
         return view;
