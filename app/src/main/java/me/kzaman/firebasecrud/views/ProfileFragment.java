e8f6ea26cb1545445a266a6fac56197420063d81
KAMRUZZAMAN <kzamanweb@gmail.com>
2021-01-23 15:00:59 +0600

    add logout option

--- app/src/main/java/me/kzaman/firebasecrud/views/ProfileFragment.java
+++ app/src/main/java/me/kzaman/firebasecrud/views/ProfileFragment.java
@@ -1,5 +1,6 @@
 package me.kzaman.firebasecrud.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import me.kzaman.firebasecrud.HomeActivity;
 import me.kzaman.firebasecrud.R;
 
 public class ProfileFragment extends Fragment {
@@ -22,6 +28,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button logoutBtn = view.findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), HomeActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                Toast.makeText(getContext(), "User Successfully Logged Out", Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            }
+        });
+        return view;
     }
 }
