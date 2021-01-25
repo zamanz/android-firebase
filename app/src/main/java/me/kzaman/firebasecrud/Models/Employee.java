5f304e143ae2615d0fe6029f3c01341365475052
KAMRUZZAMAN <kzamanweb@gmail.com>
2021-01-25 11:50:10 +0600

    add emepoyee Model setter and getter

--- app/src/main/java/me/kzaman/firebasecrud/Models/Employee.java
+++ app/src/main/java/me/kzaman/firebasecrud/Models/Employee.java
@@ -1,18 +1,21 @@
 package me.kzaman.firebasecrud.Models;
 
 public class Employee {
-    String _id, name,email,phone;
+    String name, corporate_number, personal_number, bcs_batch, blood_group, facebook, email, division_id, district_id, upazila_id, address, image;
 
-    public Employee() {
-
-    }
-
-    public String get_id() {
-        return _id;
-    }
-
-    public void set_id(String _id) {
-        this._id = _id;
+    public Employee(String name, String corporate_number, String personal_number, String bcs_batch, String blood_group, String facebook, String email, String division_id, String district_id, String upazila_id, String address, String image) {
+        this.name = name;
+        this.corporate_number = corporate_number;
+        this.personal_number = personal_number;
+        this.bcs_batch = bcs_batch;
+        this.blood_group = blood_group;
+        this.facebook = facebook;
+        this.email = email;
+        this.division_id = division_id;
+        this.district_id = district_id;
+        this.upazila_id = upazila_id;
+        this.address = address;
+        this.image = image;
     }
 
     public String getName() {
@@ -23,6 +26,46 @@ public class Employee {
         this.name = name;
     }
 
+    public String getCorporate_number() {
+        return corporate_number;
+    }
+
+    public void setCorporate_number(String corporate_number) {
+        this.corporate_number = corporate_number;
+    }
+
+    public String getPersonal_number() {
+        return personal_number;
+    }
+
+    public void setPersonal_number(String personal_number) {
+        this.personal_number = personal_number;
+    }
+
+    public String getBcs_batch() {
+        return bcs_batch;
+    }
+
+    public void setBcs_batch(String bcs_batch) {
+        this.bcs_batch = bcs_batch;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -31,11 +74,43 @@ public class Employee {
         this.email = email;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getDivision_id() {
+        return division_id;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setDivision_id(String division_id) {
+        this.division_id = division_id;
+    }
+
+    public String getDistrict_id() {
+        return district_id;
+    }
+
+    public void setDistrict_id(String district_id) {
+        this.district_id = district_id;
+    }
+
+    public String getUpazila_id() {
+        return upazila_id;
+    }
+
+    public void setUpazila_id(String upazila_id) {
+        this.upazila_id = upazila_id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
