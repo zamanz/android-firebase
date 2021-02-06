8850d1e437d598a58a95863c080e85bd8c9a823a
KAMRUZZAMAN <kzamanweb@gmail.com>
2021-02-06 16:17:00 +0600

    show data on simple listview

--- app/src/main/java/me/kzaman/firebasecrud/ui/employee_list/EmployeeListFragment.java
+++ app/src/main/java/me/kzaman/firebasecrud/ui/employee_list/EmployeeListFragment.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,6 +20,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.kzaman.firebasecrud.R;
 
 
@@ -25,7 +30,7 @@ public class EmployeeListFragment extends Fragment {
 
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     CollectionReference employees = firebaseFirestore.collection("employees");
-
+    List<String> stringList = new ArrayList<String>();
     public EmployeeListFragment() {
         // Required empty public constructor
     }
@@ -39,14 +44,19 @@ public class EmployeeListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ListView listView = view.findViewById(R.id.employees_list);
         employees.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        Log.d("employee_list", document.getId() + " => " + document.getData());
+                        Log.d("employee_list", document.getId() + " => " + document.getString("name"));
+                        stringList.add(document.getString("name"));
                         //EmployeeModel employee_list = document.toObject(EmployeeModel.class);
                     }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, stringList);
+                    arrayAdapter.notifyDataSetChanged();
+                    listView.setAdapter(arrayAdapter);
                 }
                 else {
                     Log.w("doc", "Error getting documents.", task.getException());
