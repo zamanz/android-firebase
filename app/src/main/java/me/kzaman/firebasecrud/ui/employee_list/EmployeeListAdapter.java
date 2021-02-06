cf75ee9895fd77a6a88f543591628e5ec221bc5e
KAMRUZZAMAN <kzamanweb@gmail.com>
2021-02-06 16:43:50 +0600

    Update EmployeeListAdapter.java

---
+++ app/src/main/java/me/kzaman/firebasecrud/ui/employee_list/EmployeeListAdapter.java
@@ -0,0 +1,19 @@
+package me.kzaman.firebasecrud.ui.employee_list;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import me.kzaman.firebasecrud.R;
+
+
+public class EmployeeListAdapter {
+    
+}
